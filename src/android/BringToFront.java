ce510e7a7763e870dd2607db758b0c4652e2c43c
YimasoftLiuGuanJun <35481331+YimasoftLiuGuanJun@users.noreply.github.com>
2018-01-17 10:26:42 +0800

    Update BringToFront.java

--- src/android/BringToFront.java
+++ src/android/BringToFront.java
@@ -20,7 +20,7 @@ import android.util.Log;
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 
-import in.lucasdup.bringtofront.*;
+import in.lucasdup.bringtofront.OnePixelReceiver;
 
 /**
  * This class echoes a string called from JavaScript.
