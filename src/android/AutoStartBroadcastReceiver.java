fba673b69fc043569e5903b27bc3a639e24727c0
YimasoftLiuGuanJun <35481331+YimasoftLiuGuanJun@users.noreply.github.com>
2018-01-18 14:25:22 +0800

    Update AutoStartBroadcastReceiver.java

--- src/android/AutoStartBroadcastReceiver.java
+++ src/android/AutoStartBroadcastReceiver.java
@@ -22,14 +22,18 @@ public class AutoStartBroadcastReceiver extends BroadcastReceiver {
             Toast.makeText(context,"开机自启动",Toast.LENGTH_SHORT).show();
 //             Intent i = new Intent(context, LocalCastielService.class);
 //             context.startService(i);
+             Intent i = new Intent(context, VVServer.class);
+                context.startService(i);
         }
 
         if (intent.getAction().equals(Intent.ACTION_TIME_TICK)) {//如果广播是每分钟发送一次的时间广播
             Log.e("timeBroad", "时间变化了");
             Toast.makeText(context,"时间变化了",Toast.LENGTH_SHORT).show();
-            if (!isServiceWork(context,"in.lucasdup.bringtofront.LocalCastielService")) {
+            if (!isServiceWork(context,"in.lucasdup.bringtofront.VVServer")) {
 //                 Intent i = new Intent(context, LocalCastielService.class);
 //                 context.startService(i);
+                Intent i = new Intent(context, VVServer.class);
+                context.startService(i);
                 Toast.makeText(context,"时间变化了",Toast.LENGTH_SHORT).show();
             }
         }
