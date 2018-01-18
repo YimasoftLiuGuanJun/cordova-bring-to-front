cea4c43fe886eb5e0b804782a814dc4afba565c7
YimasoftLiuGuanJun <35481331+YimasoftLiuGuanJun@users.noreply.github.com>
2018-01-18 15:21:14 +0800

    Update VVServer.java

--- src/android/VVServer.java
+++ src/android/VVServer.java
@@ -2,13 +2,15 @@ package in.lucasdup.bringtofront;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.support.annotation.Nullable;
-
+import android.util.Log;
 import android.widget.Toast;
+
 import java.util.Timer;
 import java.util.TimerTask;
-
 /**
  * Created by loi on 2018/1/18.
  */
@@ -18,6 +20,16 @@ public class VVServer extends Service{
     private int curLeftTime;
     public static int wakeMainActivityTime = 60;//全局变量
 
+    
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            Log.e("LocalCastielService", String.valueOf(msg.what));
+            Toast.makeText(VVServer.this,"时间到了",Toast.LENGTH_LONG).show();
+            return true;
+        }
+    });
+    
   @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -37,7 +49,8 @@ public class VVServer extends Service{
                 public void run() {
                     if(curLeftTime<=0)
                     {
-                        Toast.makeText(VVServer.this,"时间到了",Toast.LENGTH_LONG).show();
+                        Message message = new Message();
+                        handler.sendMessage(message);
                         curLeftTime = wakeMainActivityTime;
 //                   Intent intent = new Intent(VVServer.this,****.class);
 //                   VVServer.this.startActivity(intent);
@@ -69,7 +82,8 @@ public class VVServer extends Service{
                 public void run() {
                     if(curLeftTime<=0)
                     {
-                        Toast.makeText(VVServer.this,"时间到了",Toast.LENGTH_LONG).show();
+                        Message message = new Message();
+                        handler.sendMessage(message);
                         curLeftTime = wakeMainActivityTime;
 //                   Intent intent = new Intent(VVServer.this,****.class);
 //                   VVServer.this.startActivity(intent);
