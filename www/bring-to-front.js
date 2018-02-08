ac5be841d9c0bdabd9f9336b7fda77392ce40438
YimasoftLiuGuanJun <35481331+YimasoftLiuGuanJun@users.noreply.github.com>
2018-02-08 21:11:19 +0800

    Update bring-to-front.js

--- www/bring-to-front.js
+++ www/bring-to-front.js
@@ -1,3 +1,13 @@
-module.exports = function() {
-  cordova.exec(null, null, "bringtofront", "bringToFront", [])
-}
+var exec = require('cordova/exec');
+
+// module.exports = function() {
+//   cordova.exec(null, null, "bringtofront", "bringToFront", [])
+// }
+
+exports.test = function() {
+    exec(null, null, "bringtofront", "test", []);
+};
+
+exports.bringtofront = function() {
+   cordova.exec(null, null, "bringtofront", "bringToFront", [])
+};
